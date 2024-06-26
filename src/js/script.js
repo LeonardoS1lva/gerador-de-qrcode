d6e61cd984af8cd5e0f4ad5290ab06ecc6921987
Leonardo Silva <109092809+LeonardoS1lva@users.noreply.github.com>
2024-06-26 19:18:13 +0000

    Initial Commit

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const link = document.getElementById("conteudo");
+const btnGerar = document.getElementById("btn-gerar");
+const btnLimpar = document.getElementById("btn-limpar");
+const qrCode = document.getElementById("qrcode");
+
+function gerarQRCode() {
+  qrCode.innerHTML = "";
+  new QRCode(qrCode, { text: link.value, width: 128, height: 128 });
+}
+
+function limparLink() {
+  link.value = "";
+}
+
+btnLimpar.addEventListener("click", limparLink);
+btnGerar.addEventListener("click", gerarQRCode);
